7bf1448a05479e07d6ae0ab199ed16614bf37a5b
sibil.kurian@gmail.com <sibil.kurian@gmail.com>
2020-09-09 20:15:03 -0400

    Spring FrameWork Basics

---
+++ src/main/java/com/example/demo/Triangle.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Triangle {
+private Point p1;
+private Point p2;
+private Point p3;
+}
